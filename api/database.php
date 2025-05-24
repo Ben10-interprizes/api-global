6705ec52ddbffd40402a59a06fe39e2821bd94dd
Ben10-interprizes <binterprizes@mail2developer.com>
2025-05-24 09:34:52 +0100

    Update database.php

--- api/database.php
+++ api/database.php
@@ -3,11 +3,11 @@ header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set("Africa/Porto-Novo");
 
 try {
-    $host = "dpg-d0jt5hje5dus73b97v10-a.oregon-postgres.render.com";
-    $db   = "db_agent_ventes";
-    $user = "db_agent_ventes_user";
-    $pass = "ZSPMn6b02IEfWpESffxCYJpUUTan7pES";
-    $port = "5432";
+    $host = getenv('DB_HOST');
+    $db = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+    $port = getenv('DB_PORT');
 
     $db = new PDO(
         "pgsql:host=$host;port=$port;dbname=$db",
